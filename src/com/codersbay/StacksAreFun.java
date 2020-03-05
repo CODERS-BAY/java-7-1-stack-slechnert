95bcc7c212398b26a82acfff19da37d0838528a0
Simon <s.lechner.biz@gmail.com>
2020-03-05 16:05:01 +0100

    Fresh start

--- src/com/codersbay/StacksAreFun.java
+++ src/com/codersbay/StacksAreFun.java
@@ -1,9 +1,29 @@
 package com.codersbay;
 
 public class StacksAreFun {
+    int stack[] = new int[10];
 
     public static void main(String[] args) {
 
     }
-    
+
+    public static void push(int value) {
+
+    }
+
+    public static int size() {
+
+    }
+
+    public static int pop() {
+
+    }
+
+    public static int peek() {
+
+    }
+
+    public static int[] pop(int n) {
+
+    }
 }
