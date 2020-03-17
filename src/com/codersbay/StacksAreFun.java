b30362fcaa76b3e147f832b6947cedfec17c30f5
Simon <s.lechner.biz@gmail.com>
2020-03-17 15:39:02 +0100

    Succesfully implemented push, size, peek and output. Pop broken.
    Single pops work fine, the multiple pop method seems to do double the amount of iterations it should and I got no clue why, send help plx.

--- src/com/codersbay/StacksAreFun.java
+++ src/com/codersbay/StacksAreFun.java
@@ -1,29 +1,72 @@
 package com.codersbay;
 
+
+import java.util.List;
+import java.util.Stack;
+
 public class StacksAreFun {
-    int stack[] = new int[10];
 
     public static void main(String[] args) {
+        List<Integer> stack = new Stack<>();
+        push(stack, 1249);
+        push(stack, 1);
+        push(stack, 9);
+        push(stack, 5238);
+        push(stack, 15014354);
+        System.out.println();
 
+        output(stack);
+        System.out.println();
+
+        pop(stack);
+        peek(stack);
+        size(stack);
+        push(stack, 44);
+        push(stack, 332);
+        pop(3, stack);
+        size(stack);
+
+        //OUTPUT ALTERED CONTENT
+        System.out.println();
+        System.out.println("---------------");
+        output(stack);
     }
 
-    public static void push(int value) {
-
+    public static void push(List stack, int value) {
+        System.out.println(value + " has been added on top of the stack");
+        stack.add(value);
     }
 
-    public static int size() {
-
+    public static int size(List stack) {
+        System.out.println("Size is: " + stack.size());
+        return (stack.size());
     }
 
-    public static int pop() {
-
+    public static int pop(List stack) {
+        int tmp = (int) stack.get(stack.size() - 1);
+        System.out.println(tmp + "has been removed from the top of the stack");
+        stack.remove(stack.size() - 1);
+        return tmp;
     }
 
-    public static int peek() {
-
+    public static int peek(List stack) {
+        System.out.println("Value on top is: " + stack.get(stack.size() - 1));
+        return (int) (stack.get(stack.size() - 1));
     }
 
-    public static int[] pop(int n) {
+    public static int[] pop(int n, List stack) {
+        int[] temp = new int[n];
+        for (int j = 0; j < n; j++) {
+            pop(stack);
+            temp[j] = pop(stack);
+        }
+        return temp;
+    }
 
+    public static void output(List<Integer> stack) {
+        System.out.println("Stack values are:");
+        for (Integer value : stack) {
+            System.out.println(value);
+        }
     }
 }
