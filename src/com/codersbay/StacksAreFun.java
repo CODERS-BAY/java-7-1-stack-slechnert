bba68a537c47fdc32a472f08de4e8f741201bbbf
Simon <s.lechner.biz@gmail.com>
2020-07-06 14:30:21 +0200

    variablenames now all start with lowercase

--- src/com/codersbay/StacksAreFun.java
+++ src/com/codersbay/StacksAreFun.java
@@ -8,29 +8,29 @@ public class StacksAreFun {
 
     public static void main(String[] args) {
         List<Integer> stack = new Stack<>();
-        IntStack IntStack = new IntStack(stack);
-        IntStack.push(1249);
-        IntStack.push(1);
-        IntStack.push(9);
-        IntStack.push(5238);
-        IntStack.push(15014354);
+        IntStack intStack = new IntStack(stack);
+        intStack.push(1249);
+        intStack.push(1);
+        intStack.push(9);
+        intStack.push(5238);
+        intStack.push(15014354);
         System.out.println();
 
-        IntStack.output(stack);
+        intStack.output(stack);
         System.out.println();
 
-        IntStack.pop();
-        IntStack.peek();
-        IntStack.size();
-        IntStack.push(44);
-        IntStack.push(332);
-        IntStack.pop(3);
-        IntStack.size();
+        intStack.pop();
+        intStack.peek();
+        intStack.size();
+        intStack.push(44);
+        intStack.push(332);
+        intStack.pop(3);
+        intStack.size();
 
         //OUTPUT ALTERED CONTENT
         System.out.println();
         System.out.println("---------------");
-        IntStack.output(stack);
+        intStack.output(stack);
     }
 
 }
